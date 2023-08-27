5d58f32247cd9eb55af02a28b9b45d7cb31efabf
shovelingpig <alan.k@hpcnt.com>
2023-08-27 21:52:47 +0900

    Add advanced scoring option to inferencer

--- src/dataset_readers/inference_dsr.py
+++ src/dataset_readers/inference_dsr.py
@@ -8,18 +8,28 @@ logger = logging.getLogger(__name__)
 class InferenceDatasetReader(BaseDatasetReader):
 
     def __init__(self, index_reader, n_tokens, task_name, model_name, field, dataset_path=None,
-                 dataset_split=None, ds_size=None):
+                 dataset_split=None, ds_size=None, advanced_scoring=False):
         self.tokenizer = get_tokenizer(model_name)
         self.tokenizer.pad_token = self.tokenizer.eos_token
         self.tokenizer.pad_token_id = self.tokenizer.eos_token_id
         self.tokenizer.padding_side = "left"
         self.index_reader = index_reader
         self.n_tokens_in_prompt = n_tokens
+        self.advanced_scoring = advanced_scoring
         # set truncation to false so that metadata['len'] corresponds to metadata['text']
         self.init_dataset(task_name, field, dataset_path, dataset_split, ds_size, truncation=False)
 
     def get_ice_prompt(self, entry, prompt_len):
-        if 'ctxs' in entry:
+        if self.advanced_scoring and ('best_permutations' in entry):
+            ctx = [self.index_reader[i] for i in entry['best_permutations']]
+
+            ice_prompts_list = [i['metadata']['text'] for i in ctx]
+            ice_lengths_list = [i['metadata']['len'] for i in ctx]
+
+            trunc_ice_prompts_list = self.truncate(prompt_len, ice_lengths_list, ice_prompts_list)
+            ice_separator = self.dataset_wrapper.ice_separator
+            ice_prompt = ice_separator.join(trunc_ice_prompts_list) + ice_separator
+        elif 'ctxs' in entry:
             ctx = [self.index_reader[i] for i in entry['ctxs']]
 
             ice_prompts_list = [i['metadata']['text'] for i in ctx]
@@ -45,8 +55,12 @@ class InferenceDatasetReader(BaseDatasetReader):
         entry['prompt'] = prompt + self.dataset_wrapper.a_prefix
         entry['ice_prompts_list'] = trunc_ice_prompts_list
 
-        tokenized_example = self.tokenizer.encode_plus(entry['prompt'], truncation=False, return_tensors='pt',
-                                                       add_special_tokens=False)
+        tokenized_example = self.tokenizer.encode_plus(
+            entry['prompt'],
+            truncation=False,
+            return_tensors='pt',
+            add_special_tokens=False
+        )
 
         return {
             'input_ids': tokenized_example.input_ids[0],
